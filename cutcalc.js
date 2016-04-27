66fac0e0098907781be3513d86de76f9cf646602
Philip John <phil@philipjohn.co.uk>
2016-04-27 22:19:14 +0100

    Start to make the years dynamic

--- cutcalc.js
+++ cutcalc.js
@@ -13,20 +13,13 @@ $(function(){
 
 		// Grab the salary
 		var salary = $('#gross_salary').val();
+		var taxable_income = {};
+		var tax = {};
 
 		// What are the tax-free allowances?
-		var allowance_2016 = 10600;
-		var allowance_2017 = 11000;
-
-		// What are the tax rates?
-		var tax_rates_2016 = {
-			31785: 0.2,
-			150000: 0.4
-		};
-		var tax_rates_2017 = {
-			32000: 0.2,
-			150000: 0.4
-		};
+		var allowance = {};
+			allowance["2016"] = 10600;
+			allowance["2017"] = 11000;
 
 		// Check that salary is actually an integer
 		if ( ! isNormalInteger( salary ) ) {
@@ -34,36 +27,36 @@ $(function(){
 			return;
 		}
 
-		// Calculate the 2016 taxable income
+		// Calculate the taxable income
 		// Don't tax folks earning up to the tax-free allowance
-		if ( salary <= allowance_2016 ) {
-			var taxable_income_2016 = 0;
+		if ( salary <= allowance["2016"] ) {
+			taxable_income["2016"] = 0;
 		}
 		// Everyone else, under £100,001 is pretty standard
 		else if ( salary < 100001 ) {
-			var taxable_income_2016 = salary - allowance_2016;
+			taxable_income["2016"] = salary - allowance["2016"];
 		}
 		// Above £100k, the allowance gradually decreases
 		else if ( salary < 122000 ) {
 			var over_100 = salary - 100000;
-			var adjusted_allowance = allowance_2016 - ( over_100 / 2 );
-			var taxable_income_2016 = salary - adjusted_allowance;
+			var adjusted_allowance = allowance["2016"] - ( over_100 / 2 );
+			taxable_income["2016"] = salary - adjusted_allowance;
 		}
 		// Above £100k, there is no allowance
 		else {
-			var taxable_income_2016 = salary;
+			taxable_income["2016"] = salary;
 		}
 
 		// Get the tax rate and tax amount
-		if ( taxable_income_2016 <= 31785 ) {
-			var tax_2016 = taxable_income_2016 * 0.2;
-		} else if ( taxable_income_2016 <= 150000 ) {
-			var tax_2016 = 31785 * 0.2;
-				tax_2016 += ( taxable_income_2016 - 31785 ) * 0.4;
+		if ( taxable_income["2016"] <= 31785 ) {
+			tax["2016"] = taxable_income["2016"] * 0.2;
+		} else if ( taxable_income["2016"] <= 150000 ) {
+			tax["2016"] = 31785 * 0.2;
+			tax["2016"] += ( taxable_income["2016"] - 31785 ) * 0.4;
 		} else {
-			var tax_2016 = 31785 * 0.2; // basic rate
-				tax_2016 += 118215 * 0.4; // higher rate
-				tax_2016 += ( taxable_income_2016 - 150000 ) * 0.45;
+			tax["2016"] = 31785 * 0.2; // basic rate
+			tax["2016"] += 118215 * 0.4; // higher rate
+			tax["2016"] += ( taxable_income["2016"] - 150000 ) * 0.45;
 		}
 
 	});
