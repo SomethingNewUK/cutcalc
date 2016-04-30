b284a20c050663249b83528c519fbaea4154c2a4
Philip John <phil@philipjohn.co.uk>
2016-04-30 14:26:46 +0100

    Separate out the calculations from the specific year

--- cutcalc.js
+++ cutcalc.js
@@ -13,53 +13,83 @@ $(function(){
 
 		// Grab the salary
 		var salary = $('#gross_salary').val();
-		var taxable_income = {};
-		var tax = {};
-
-		// What are the tax-free allowances?
-		var allowance = {};
-			allowance["2016"] = 10600;
-			allowance["2017"] = 11000;
-
 		// Check that salary is actually an integer
 		if ( ! isNormalInteger( salary ) ) {
 			console.error( "Salary isn't a number!" );
 			return;
 		}
 
+		var tax_2016 = get_tax_for_year( 2016, salary );
+		var tax_2017 = get_tax_for_year( 2017, salary );
+
+		// How much less tax for 2017?
+		var tax_diff = tax_2016 - tax_2017;
+
+		if ( tax_diff > 0 ) {
+			// Do whatever
+		} else {
+			// You're better off, congratulations!
+		}
+
+	});
+
+	/**
+	 * Calculate the tax for a tax year, based on a salary
+	 *
+	 * @param year int The year in which the tax year we want finishes (e.g. 2016-17 would be "2017")
+	 * @param salary int The salary we need to get the tax amount for
+	 */
+	function get_tax_for_year( year, salary ) {
+
+		// Storage for later
+		var taxable_income = {};
+		var tax = {};
+
+		// What are the tax-free allowances?
+		var allowance = {};
+		allowance["2016"] = 10600;
+		allowance["2017"] = 11000;
+
 		// Calculate the taxable income
 		// Don't tax folks earning up to the tax-free allowance
-		if ( salary <= allowance["2016"] ) {
-			taxable_income["2016"] = 0;
+		if ( salary <= allowance[ year ] ) {
+			taxable_income[ year ] = 0;
 		}
 		// Everyone else, under £100,001 is pretty standard
 		else if ( salary < 100001 ) {
-			taxable_income["2016"] = salary - allowance["2016"];
+			taxable_income[ year ] = salary - allowance[ year ];
 		}
 		// Above £100k, the allowance gradually decreases
 		else if ( salary < 122000 ) {
 			var over_100 = salary - 100000;
-			var adjusted_allowance = allowance["2016"] - ( over_100 / 2 );
-			taxable_income["2016"] = salary - adjusted_allowance;
+			var adjusted_allowance = allowance[ year ] - ( over_100 / 2 );
+			taxable_income[ year ] = salary - adjusted_allowance;
 		}
 		// Above £100k, there is no allowance
 		else {
-			taxable_income["2016"] = salary;
+			taxable_income[ year ] = salary;
 		}
+
+		// The size of the gap between the tax free allowance and the next tax bracket
+		var tax_bracket = {};
+			tax_bracket["2016"] = 31785;
+			tax_bracket["2017"] = 32000;
 
 		// Get the tax rate and tax amount
-		if ( taxable_income["2016"] <= 31785 ) {
-			tax["2016"] = taxable_income["2016"] * 0.2;
-		} else if ( taxable_income["2016"] <= 150000 ) {
-			tax["2016"] = 31785 * 0.2;
-			tax["2016"] += ( taxable_income["2016"] - 31785 ) * 0.4;
+		if ( taxable_income[ year ] <= tax_bracket[ year ] ) {
+			tax = taxable_income[ year ] * 0.2;
+		} else if ( taxable_income[ year ] <= 150000 ) {
+			tax = tax_bracket[ year ] * 0.2;
+			tax += ( taxable_income[ year ] - tax_bracket[ year ] ) * 0.4;
 		} else {
-			tax["2016"] = 31785 * 0.2; // basic rate
-			tax["2016"] += 118215 * 0.4; // higher rate
-			tax["2016"] += ( taxable_income["2016"] - 150000 ) * 0.45;
+			tax = tax_bracket[ year ] * 0.2; // basic rate
+			tax += ( 150000 - tax_bracket[ year ] ) * 0.4; // higher rate
+			tax += ( taxable_income[ year ] - 150000 ) * 0.45;
 		}
 
-	});
+		return tax;
+
+	}
 
 	// Validate integers
 	// http://stackoverflow.com/questions/10834796/validate-that-a-string-is-a-positive-integer
